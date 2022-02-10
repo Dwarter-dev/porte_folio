a83e70a4ad7e451e82bc114c1b8edee0f9538394
Steven Da Costa <60145033+Dwarter-dev@users.noreply.github.com>
2022-02-10 17:06:50 +0100

    Ajout du controller porte_folio + twig

--- src/Controller/PorteFolioController.php
+++ src/Controller/PorteFolioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class PorteFolioController extends AbstractController
 {
     #[Route('/porte/folio', name: 'porte_folio')]
-    public function index(): Response
+    public function index(ProjetRepository $projetRepository): Response
     {
+        $projet = $projetRepository->findAll();
+
         return $this->render('porte_folio/index.html.twig', [
-            'controller_name' => 'PorteFolioController',
+            'projets' => $projet,
         ]);
     }
 }
